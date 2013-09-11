6ab232774d70ae214d9013e5933fd4c389a0dc3c
Alejandro Rosales <karnal02@gmail.com>
2013-09-11 13:55:49 -0500

    firts commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Picturespinball::Application.config.secret_token = '8df42ae80927681321623ed787400afdbba4af2a219bf0d3f83c50ca3d776b9c895daec8ec0314ca31fa9fc1cec77585af099c8b37446583ebe912abc6a76384'
